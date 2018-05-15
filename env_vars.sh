f6a3b3167f5cc39a2d2dc4d24a6a41b04871e1d9
Matthias Endler <matthias-endler@gmx.net>
2018-05-15 10:32:52 +0200

    Delete env_vars.sh

--- env_vars.sh
+++
@@ -1,7 +0,0 @@
-export CONTAINER_DIR=/myapp
-export SOURCE_DIR=/Users/me/myapp
-
-# by Pascal Andy
-# https://pascalandy.com/blog/now/
-# https://twitter.com/askpascalandy
-# https://github.com/pascalandy/
